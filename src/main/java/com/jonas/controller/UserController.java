bb1f2d537f95ef9aadf6eff2a770114fa4a0589c
shenjy <shenjy24@gmail.com>
2022-07-05 10:21:45 +0800

    postForObject

--- src/main/java/com/jonas/controller/UserController.java
+++ src/main/java/com/jonas/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.jonas.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,5 +32,13 @@ public class UserController {
            put("userId", userId);
         }};
         return restTemplate.getForObject(API, String.class, args);
+    }
+
+    @PostMapping("postName")
+    public String postName(Integer userId) {
+        MultiValueMap<String, Integer> request = new LinkedMultiValueMap<>();
+        request.add("userId", userId);
+        URI uri = URI.create("http://service-provider/user/getUserName");
+        return restTemplate.postForObject(uri, request, String.class);
     }
 }
